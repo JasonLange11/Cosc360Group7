f876f007b2de6120b7e815d58be087a968ea355a
A-Raber <araber@student.ubc.ca>
2026-03-02 12:54:32 -0800

    Added a basic header to the components, still needs Navbar component that the header will use

---
+++ react-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header(){
+
+    return (
+        <header>
+            <h1>
+                TouchGrassEvents
+            </h1>
+            <Navbar />
+        </header>
+    );
+}
+
+export default Header;
