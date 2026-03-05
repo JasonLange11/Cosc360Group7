230d6996143de40b679a8394d3ba80554a33e778
Daniel Williams <budxors@gmail.com>
2026-03-05 10:35:45 -0800

    Added a search bar component

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import SearchBar from './components/SearchBar'
 
 function App() {
 
   return (
     <>
       <Header />
+      <SearchBar />
     </>
   )
 }
