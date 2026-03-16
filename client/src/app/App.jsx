8cbcfd54b7947c12e71330cb301c3f9daed30f9a
A-Raber <araber@student.ubc.ca>
2026-03-16 10:56:17 -0700

    adding new routes on the homepage

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,15 +1,17 @@
 // eslint-disable-next-line no-unused-vars
-import { useState } from 'react'
 import './App.css'
 import Header from '../components/Header'
 import SearchBar from '../components/SearchBar'
 import Footer from '../components/Footer'
 import Sidebar from '../components/Sidebar'
 import CardDisplay from '../components/CardDisplay'
+import {Routes, Route} from 'react-router-dom'
+import Login from '../features/auth/Login'
+import Signup from '../features/auth/Registration'
 
 const card1Image = {src:"/src/assets/bookclub.jpg", alt:"A stack of books with the words 'Book Club'"};
 
-function App() {
+function HomePage() {
 
   return (
     <>
@@ -30,4 +32,12 @@ function App() {
   )
 }
 
-export default App
+export default function App(){
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  )
+}
