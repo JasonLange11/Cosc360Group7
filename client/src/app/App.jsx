7b12854877786e921214415c4b689ff13d8edeac
A-Raber <araber@student.ubc.ca>
2026-03-18 13:02:16 -0700

    updated the app to implement EventList instead plus removed hardcoded data

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,16 +1,14 @@
-// eslint-disable-next-line no-unused-vars
+
 import './App.css'
 import Header from '../components/Header'
 import SearchBar from '../components/SearchBar'
 import Footer from '../components/Footer'
 import Sidebar from '../components/Sidebar'
-import CardDisplay from '../components/CardDisplay'
+import EventsList from '../components/EventsList'
 import { Navigate, Routes, Route } from 'react-router-dom'
 import Login from '../features/auth/Login'
 import Signup from '../features/auth/Registration'
 import { useAuth } from '../context/AuthContext.jsx'
-
-const card1Image = {src:"/src/assets/bookclub.jpg", alt:"A stack of books with the words 'Book Club'"};
 
 function HomePage() {
 
@@ -19,15 +17,7 @@ function HomePage() {
       <Header />
       <SearchBar />
       <Sidebar />
-      <CardDisplay 
-      img={card1Image} 
-      heading="Book Club Meeting" 
-      details={[
-        ["/src/assets/calendar.svg", "August 19"], 
-        ["/src/assets/clock.svg", "5:00pm PST"], 
-        ["/src/assets/location.svg", "Okanagan Regional Library"]]
-      } 
-        description="Weekly Kelowna Book Club meet-up"/>
+      <EventsList />
       <Footer />
     </>
   )
