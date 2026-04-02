d75bbd9eb3c0ff0677c35fb1c47cb8d68d0f1cc4
Moop333 <Moop3334@gmail.com>
2026-04-01 22:44:58 -0700

    implemented the repository file for groups

---
+++ server/src/modules/groups/groups.respository.js
@@ -0,0 +1,25 @@
+import { Group  } from "./groups.model.js";
+
+export async function createEvent(groupData) {
+  return Group.create(groupData);
+}
+
+export async function getAllGroups() {
+  return Group.find().sort({ createdAt: -1 }).lean(); //NOTE: Why is lean used here but not elsewhere?
+}
+
+export async function getGroupById(groupId) {
+  return Group.findById(groupId);
+}
+
+export async function updateGroupById(groupId, updateData) {
+  return Group.findByIdAndUpdate(groupId, updateData, {new: true, runValidators: true, });
+}
+
+export async function deleteGroupById(groupId) {
+  return Group.findByIdAndDelete(groupId);
+}
+
+export async function getGroupsByUserId(userId) {
+  return Group.find({ userId }).sort({ createdAt: -1 }).lean();
+}
