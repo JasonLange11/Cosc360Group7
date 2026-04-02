9b392d98dab2a5d164472674dd5dd8f48762acbc
Daniel Williams <budxors@gmail.com>
2026-04-02 11:10:06 -0700

    changed homepage to 2 column layout

--- client/src/components/ui/Sidebar.jsx
+++ client/src/components/ui/Sidebar.jsx
@@ -3,14 +3,14 @@ import './css/Sidebar.css'
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h2>
-                Groups:
-            </h2>
-            <ul className='side list'>
-                <li><a href='#'>Group 1</a></li>
-                <li><a href='#'>Group 2</a></li>
-                <li><a href='#'>Group 3</a></li>
-            </ul>
+            <section className='sidebar-panel'>
+                <h2>Groups:</h2>
+                <ul className='side list'>
+                    <li><a href='#'>Group 1</a></li>
+                    <li><a href='#'>Group 2</a></li>
+                    <li><a href='#'>Group 3</a></li>
+                </ul>
+            </section>
         </div>
     ) //Need to modify this to dynamically update the displayed groups at some point
 }
