c2187a5252c7f34ca5d292d76f9ad812b35cd06a
A-Raber <araber@student.ubc.ca>
2026-04-02 17:12:59 -0700

    removed the swagger docs from the project

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,6 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./data/db/connection.js"
-import swaggerUi from "swagger-ui-express";
-import swaggerJSDoc from "swagger-jsdoc";
 import usersRouter from "./modules/users/users.routes.js";
 import authRouter from "./modules/auth/auth.routes.js";
 import eventsRouter from "./modules/events/events.routes.js";
@@ -11,20 +9,6 @@ import { errorHandler } from "./middleware/error-handler.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Cosc 360 API",
-      version: "1.0.0",
-      description: "Backend API documentation"
-    }
-  },
-  apis: ["./src/modules/users/*.js", "./src/modules/auth/*.js", "./src/modules/events/*.js", "./src/index.js"]
-};
-
-const specs = swaggerJSDoc(swaggerOptions);
 
 app.use(cors());
 app.use(express.json());
@@ -36,7 +20,6 @@ app.get("/", (req, res) => {
 app.use("/api/users", usersRouter);
 app.use("/api/auth", authRouter)
 app.use("/api/events", eventsRouter)
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use(errorHandler);
 
 
@@ -46,7 +29,6 @@ async function startServer(){
 
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-    console.log(`Server docs running on http://localhost:${PORT}/docs`);
   });
 }
 
--- server/src/modules/users/users.routes.js
+++ server/src/modules/users/users.routes.js
@@ -4,17 +4,6 @@ import { authenticateUser } from "../../middleware/auth.js";
 
 const router = Router();
 
-/**
- * @openapi
- * /api/users:
- *   get:
- *     summary: Get all users
- *     description: Returns a list of users
- *     responses:
- *       200:
- *         description: A list of users
- */
-
 router.get("/", getUsers);
 
 router.post("/", createUserAccount);
