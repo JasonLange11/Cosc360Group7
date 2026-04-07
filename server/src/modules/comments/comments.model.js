c9a882abaf74e2ffa93a661d5b7dea0ef50bd808
Moop333 <Moop3334@gmail.com>
2026-04-06 18:33:14 -0700

    Create a comment schema in the backend

---
+++ server/src/modules/comments/comments.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    groupId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Group",
+      required: true,
+      index: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 1000,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);
