3ee782be2db630fb40ba5977fc5e1514d1cd9997
A-Raber <araber@student.ubc.ca>
2026-04-07 23:34:02 -0700

    adding calls for updating users status and info for admins

--- client/src/lib/usersApi.js
+++ client/src/lib/usersApi.js
@@ -2,13 +2,39 @@ import { apiRequest } from './apiClient';
 import { getAuthHeader } from './auth';
 
 export async function getUsers() {
-  return apiRequest('/api/users');
+  return apiRequest('/api/users', {
+    headers: getAuthHeader('You must be logged in to view users'),
+  });
 }
 
 export async function deleteUser(userId){
   return apiRequest(`/api/users/${userId}`, {
     method: 'DELETE',
     headers: getAuthHeader('You must be logged in to remove a user'),
+  })
+}
+
+export async function updateUserStatus(userId, isDisabled) {
+  return apiRequest(`/api/users/${userId}/status`, {
+    method: 'PATCH',
+    headers: getAuthHeader('You must be logged in to update a user'),
+    body: JSON.stringify({ isDisabled }),
+  })
+}
+
+export async function updateUserAdmin(userId, isAdmin) {
+  return apiRequest(`/api/users/${userId}/admin`, {
+    method: 'PATCH',
+    headers: getAuthHeader('You must be logged in to update a user'),
+    body: JSON.stringify({ isAdmin }),
+  })
+}
+
+export async function updateUserProfile(userId, profileData) {
+  return apiRequest(`/api/users/${userId}/profile`, {
+    method: 'PUT',
+    headers: getAuthHeader('You must be logged in to update a user'),
+    body: JSON.stringify(profileData),
   })
 }
 
